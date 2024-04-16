d89b1d30d3640f4af4b6b9c22d1bd135b4692830
Ferenandios <www.terunplay@gmail.com>
2024-04-16 20:58:51 +0300

    Created Icon-component in Input-component

---
+++ src/components/Box/Input/Icon/Icon.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+const Icon: FC = (): JSX.Element => {
+  return (
+    <>
+      <div>
+        {/* REACT ICONS  */}
+      </div>
+    </>
+  );
+};
+
+export default Icon;
