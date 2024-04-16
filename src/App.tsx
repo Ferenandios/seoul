1e8d1cfaaa3976d8437dcb9c34665013cb848033
Ferenandios <www.terunplay@gmail.com>
2024-04-16 20:56:38 +0300

    Created Ansoc & Inner stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { FC } from "react";
+import Title from "./components/Title/Title";
+import Box from "./components/Box/Box";
 
 const App: FC = (): JSX.Element => {
   return (
     <>
-      <div className="bg-lime-200">works!</div>
+      <main className="flex justify-center items-center w-screen min-h-screen bg-[#F5F5F5]">
+        <div className="flex flex-col w-[600px] min-h-[600px] px-[16px] pb-[8px]">
+          <Title />
+          <Box />
+        </div>
+      </main>
     </>
   );
 };
