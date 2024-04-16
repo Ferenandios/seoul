6099ca6c2f31216afe6ca5fb1cc9ca504c507062
Ferenandios <www.terunplay@gmail.com>
2024-04-16 20:57:57 +0300

    Created Input-component

---
+++ src/components/Box/Input/Input.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+
+const Input: FC = (): JSX.Element => {
+  return (
+    <>
+      <div className="flex gap-[16px]">
+        {/* ADD HERE THE ICON COMPONENT
+        AND HERE THE INPUT.. */}
+      </div>
+    </>
+  );
+};
+
+export default Input;
