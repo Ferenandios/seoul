bf1765ff381aec6ed2a9cc70065df4b2e384bb64
Ferenandios <www.terunplay@gmail.com>
2024-04-17 07:57:11 +0300

    Added imported styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
 import Title from "./components/Title/Title";
 import Box from "./components/Box/Box";
+import styles from "./App.module.css";
 
 const App: FC = (): JSX.Element => {
   return (
     <>
-      <main className="flex justify-center items-center w-screen min-h-screen bg-[#F5F5F5]">
-        <div className="flex flex-col w-[600px] min-h-[600px] px-[16px] pb-[8px]">
+      <main className={styles.ansoc}>
+        <div className={styles.inner}>
           <Title />
           <Box />
         </div>
