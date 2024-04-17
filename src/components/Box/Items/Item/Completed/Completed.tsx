dea3e518c3b35bbdd109563121901e0d5e0d6199
Ferenandios <www.terunplay@gmail.com>
2024-04-17 07:54:49 +0300

    Created Completed-component in Items-component

---
+++ src/components/Box/Items/Item/Completed/Completed.tsx
@@ -0,0 +1,28 @@
+import { FC } from "react";
+import { GiCheckMark } from "react-icons/gi";
+import { type IItem } from "../../../../../types/types";
+import styles from "./Completed.module.css";
+import { useAppDispatch } from "../../../../../hooks/hooks";
+import { toggleItem } from "../../../../../features/main.slice";
+
+const Completed: FC<{ item: IItem }> = ({ item }): JSX.Element => {
+  const dispatch = useAppDispatch();
+  const handleClick = () => {
+    dispatch(toggleItem(item.id - 1));
+  };
+  return (
+    <>
+      <div className={styles.ansoc}>
+        <button
+          onClick={handleClick}
+          style={{ borderColor: item.completed ? "#A4D5C9" : "" }}
+          className={styles.inner}
+        >
+          {item.completed && <GiCheckMark size={16} fill="#A4D5C9" />}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Completed;
