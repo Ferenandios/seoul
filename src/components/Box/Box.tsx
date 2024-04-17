933adbb3bea27e52daab7be41b77aa6850c619ff
Ferenandios <www.terunplay@gmail.com>
2024-04-17 14:06:50 +0300

    Added Manage-component

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import styles from "./Box.module.css";
 import Input from "./Input/Input";
 import Items from "./Items/Items";
+import Manage from "./Manage/Manage";
 
 const Box: FC = (): JSX.Element => {
   return (
@@ -9,6 +10,7 @@ const Box: FC = (): JSX.Element => {
       <div className={styles.inner}>
         <Input />
         <Items />
+        <Manage />
       </div>
     </>
   );
