1ac6c6424b839bdc1b70f6fd9f29205f9df967a9
Ferenandios <www.terunplay@gmail.com>
2024-04-17 12:46:18 +0300

    Added filter-by-button.type

--- src/components/Box/Items/Items.tsx
+++ src/components/Box/Items/Items.tsx
@@ -2,15 +2,23 @@ import { FC } from "react";
 import { useAppSelector } from "../../../hooks/hooks";
 import Item from "./Item/Item";
 import styles from "./Items.module.css";
+import { type IItem } from "../../../types/types";
 
 const Items: FC = (): JSX.Element => {
-  const { items } = useAppSelector((state) => state);
+  const { items, filterBy } = useAppSelector((state) => state);
+  const filterFunc = (item: IItem): boolean => {
+    if (filterBy === "Active") return !item.completed;
+    if (filterBy === "Completed") return item.completed;
+    return true; // filterBy === 'All'
+  };
   return (
     <>
       <div className={styles.inner}>
-        {items.map((item) => (
-          <Item key={item.id} item={item} />
-        ))}
+        {items
+          .filter((item) => filterFunc(item))
+          .map((item) => (
+            <Item key={item.id} item={item} />
+          ))}
       </div>
     </>
   );
