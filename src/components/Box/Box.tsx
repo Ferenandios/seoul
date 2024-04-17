9e33476298ea99c57f4979d92bc8f8820974be96
Ferenandios <www.terunplay@gmail.com>
2024-04-17 07:56:59 +0300

    Added Items-component

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,12 +1,14 @@
 import { FC } from "react";
 import styles from "./Box.module.css";
 import Input from "./Input/Input";
+import Items from "./Items/Items";
 
 const Box: FC = (): JSX.Element => {
   return (
     <>
       <div className={styles.inner}>
         <Input />
+        <Items />
       </div>
     </>
   );
