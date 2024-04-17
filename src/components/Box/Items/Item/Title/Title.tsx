e1c8ef11db38b3e9b21f430e665cc164056f5030
Ferenandios <www.terunplay@gmail.com>
2024-04-17 07:53:07 +0300

    Created Title-component in Items-component

---
+++ src/components/Box/Items/Item/Title/Title.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import { IItem } from "../../../../../types/types";
+import styles from "./Title.module.css";
+
+const Title: FC<{ item: IItem }> = ({ item }): JSX.Element => {
+  return (
+    <>
+      <div className={styles.ansoc}>
+        <h1
+          style={{
+            color: item.completed ? "#EDEDED" : "",
+            textDecoration: item.completed ? "line-through" : "",
+            textDecorationColor: "#EDEDED",
+          }}
+          className={styles.title}
+        >
+          {item.title}
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default Title;
