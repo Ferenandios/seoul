d1c4e170429f7c459ae051e88c32a6bc7ec737d4
Ferenandios <www.terunplay@gmail.com>
2024-04-17 12:45:42 +0300

    Added dispatch setItems for a button

--- src/components/Manage/Clear/Clear.tsx
+++ src/components/Manage/Clear/Clear.tsx
@@ -1,11 +1,20 @@
 import { FC } from "react";
+import { useAppDispatch, useAppSelector } from "../../../hooks/hooks";
+import { setItems } from "../../../features/main.slice";
 
 const Clear: FC = (): JSX.Element => {
-  const handleClick = () => {};
+  const dispatch = useAppDispatch();
+  const { items } = useAppSelector((state) => state);
+  const handleClick = () => {
+    dispatch(setItems(items.filter((item) => !item.completed)));
+  };
   return (
     <>
       <div className="flex items-center h-full">
-        <button onClick={handleClick} className="text-black text-opacity-40">
+        <button
+          onClick={handleClick}
+          className="text-black text-opacity-40 max-w-[150px] whitespace-nowrap overflow-hidden overflow-ellipsis"
+        >
           Clear completed
         </button>
       </div>
