b48179f19ce5fc06dae87bb3e9a4b14c892204df
Ferenandios <www.terunplay@gmail.com>
2024-04-17 07:56:46 +0300

    Added onKeyUp, onChange and value attributes in input

--- src/components/Box/Input/Field/Field.tsx
+++ src/components/Box/Input/Field/Field.tsx
@@ -1,14 +1,26 @@
 import { FC } from "react";
+import { addItem, setValue } from "../../../../features/main.slice";
+import { useAppDispatch, useAppSelector } from "../../../../hooks/hooks";
 import styles from "./Field.module.css";
 
 const Field: FC = (): JSX.Element => {
-  const handleKeyUp = () => {
-    
+  const dispatch = useAppDispatch();
+  const { value } = useAppSelector((state) => state);
+  const handleKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key !== "Enter") return;
+    if (event.currentTarget.value.trim() === "") return;
+    dispatch(addItem(value));
+    dispatch(setValue(""));
+  };
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setValue(event.currentTarget.value));
   };
   return (
     <>
       <input
-        onKeyUp={handleKeyUp}
+        onKeyUp={(event) => handleKeyUp(event)}
+        onChange={(event) => handleChange(event)}
+        value={value}
         className={styles.input}
         placeholder="What needs to be done?"
         type="text"
